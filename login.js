87f6b5843a3b41f1d9871f4f0fd02a0a428579e4
Nathanos56 <nthnlwwlsn@gmail.com>
2024-02-22 19:14:34 -0700

    finished sign in animation

--- login.js
+++ login.js
@@ -1,16 +1,16 @@
 // Select both username and password input fields
-const inputFields = document.querySelectorAll('.form-control');
+// const inputFields = document.querySelectorAll('.form-control');
 
-for (const inputField of inputFields) {
-  const placeholderText = inputField.nextElementSibling;
+// for (const inputField of inputFields) {
+//   const placeholderText = inputField.nextElementSibling;
 
-  inputField.addEventListener('focus', () => {
-    placeholderText.style.opacity = 0.5;
-    placeholderText.style.transform = 'translate(-7px, -25px) scale(0.8)';
-  });
+//   inputField.addEventListener('focus', () => {
+//     placeholderText.style.opacity = 0.5;
+//     placeholderText.style.transform = 'translate(-7px, -25px) scale(0.8)';
+//   });
 
-  inputField.addEventListener('blur', () => {
-    placeholderText.style.opacity = 0.7;
-    placeholderText.style.transform = 'translate(0px, 0px) scale(1)';
-  });
-}
+//   inputField.addEventListener('blur', () => {
+//     placeholderText.style.opacity = 0.7;
+//     placeholderText.style.transform = 'translate(0px, 0px) scale(1)';
+//   });
+// }
